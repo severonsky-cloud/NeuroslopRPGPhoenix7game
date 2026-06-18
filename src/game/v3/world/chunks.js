8177567f0fa0cc06bd5e02cda252946241e86f87
severonsky-cloud <severonsky@gmail.com>
2026-06-18 13:07:57 +0300

    Expand v3 instanced chunks across new large world

--- src/game/v3/world/chunks.js
+++ src/game/v3/world/chunks.js
@@ -1,5 +1,5 @@
 import { createInstancedRocks, createInstancedTsarborTrees, createInstancedGlassTrees, placeRoad } from './props.js';
-import { ROADS } from '../data/worldData.js';
+import { ROADS, WORLD_BOUNDS } from '../data/worldData.js';
 
 function seededRandom(seed) {
   let s = seed >>> 0;
@@ -29,8 +29,8 @@ export class WorldChunks {
     const rocks = [];
     for (let i = 0; i < rockCount; i++) {
       rocks.push({
-        x: -80 + rnd() * 340,
-        z: -80 + rnd() * 340,
+        x: WORLD_BOUNDS.minX + 55 + rnd() * (WORLD_BOUNDS.maxX - WORLD_BOUNDS.minX - 110),
+        z: WORLD_BOUNDS.minZ + 40 + rnd() * (WORLD_BOUNDS.maxZ - WORLD_BOUNDS.minZ - 80),
         s: 0.12 + rnd() * 0.7,
         ry: rnd() * 6.28,
       });
@@ -40,7 +40,7 @@ export class WorldChunks {
     const treeCount = this.quality?.instancedTsarborTrees ?? 78;
     const tsarbor = [];
     for (let i = 0; i < treeCount; i++) {
-      tsarbor.push({ x: 112 + rnd() * 100, z: 130 + rnd() * 90, h: 6 + rnd() * 7, ry: rnd() * 6.28 });
+      tsarbor.push({ x: 128 + rnd() * 100, z: 218 + rnd() * 92, h: 6 + rnd() * 7, ry: rnd() * 6.28 });
     }
     const trees = createInstancedTsarborTrees(this.scene, tsarbor);
     this.objects.push(trees.trunks, trees.crowns);
@@ -48,7 +48,7 @@ export class WorldChunks {
     const glassCount = this.quality?.instancedGlassTrees ?? 66;
     const glass = [];
     for (let i = 0; i < glassCount; i++) {
-      glass.push({ x: 170 + rnd() * 90, z: 18 + rnd() * 96, h: 4 + rnd() * 8, ry: rnd() * 6.28 });
+      glass.push({ x: 246 + rnd() * 92, z: 72 + rnd() * 88, h: 4 + rnd() * 8, ry: rnd() * 6.28 });
     }
     this.objects.push(createInstancedGlassTrees(this.scene, glass));
   }
