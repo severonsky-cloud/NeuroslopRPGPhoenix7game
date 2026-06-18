9e8ff31306cc040d055d459b60c26878d5ed8bcb
severonsky-cloud <severonsky@gmail.com>
2026-06-18 14:20:21 +0300

    Add v3H Windows launcher

---
+++ START_GAME_V3H_WINDOWS.bat
@@ -0,0 +1,7 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+start "Phoenix7 v3H local server" powershell -NoExit -ExecutionPolicy Bypass -File "%~dp0tools\phoenix_server.ps1" -Port 8000
+timeout /t 2 /nobreak >nul
+start "" "http://localhost:8000/game_v3h.html"
+endlocal
