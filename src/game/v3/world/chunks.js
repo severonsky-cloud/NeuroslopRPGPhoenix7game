79e529f00d8364430daad7128ba4308e44401781
severonsky-cloud <severonsky@gmail.com>
2026-06-18 11:07:04 +0300

    Wire v3 chunks to quality preset counts

--- src/game/v3/world/chunks.js
+++ src/game/v3/world/chunks.js
@@ -10,8 +10,9 @@ function seededRandom(seed) {
 }
 
 export class WorldChunks {
-  constructor(scene) {
+  constructor(scene, quality) {
     this.scene = scene;
+    this.quality = quality;
     this.loaded = false;
     this.objects = [];
   }
@@ -23,8 +24,10 @@ export class WorldChunks {
     for (const road of ROADS) placeRoad(this.scene, road, 5.0);
 
     const rnd = seededRandom(7331);
+
+    const rockCount = this.quality?.instancedRocks ?? 520;
     const rocks = [];
-    for (let i = 0; i < 650; i++) {
+    for (let i = 0; i < rockCount; i++) {
       rocks.push({
         x: -80 + rnd() * 340,
         z: -80 + rnd() * 340,
@@ -34,15 +37,17 @@ export class WorldChunks {
     }
     this.objects.push(createInstancedRocks(this.scene, rocks));
 
+    const treeCount = this.quality?.instancedTsarborTrees ?? 78;
     const tsarbor = [];
-    for (let i = 0; i < 95; i++) {
+    for (let i = 0; i < treeCount; i++) {
       tsarbor.push({ x: 112 + rnd() * 100, z: 130 + rnd() * 90, h: 6 + rnd() * 7, ry: rnd() * 6.28 });
     }
     const trees = createInstancedTsarborTrees(this.scene, tsarbor);
     this.objects.push(trees.trunks, trees.crowns);
 
+    const glassCount = this.quality?.instancedGlassTrees ?? 66;
     const glass = [];
-    for (let i = 0; i < 86; i++) {
+    for (let i = 0; i < glassCount; i++) {
       glass.push({ x: 170 + rnd() * 90, z: 18 + rnd() * 96, h: 4 + rnd() * 8, ry: rnd() * 6.28 });
     }
     this.objects.push(createInstancedGlassTrees(this.scene, glass));
