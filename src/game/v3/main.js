dd106dfc79300291fefe1daaa7c9b3737e6907a0
severonsky-cloud <severonsky@gmail.com>
2026-06-18 10:52:51 +0300

    Add v3 main entrypoint

---
+++ src/game/v3/main.js
@@ -0,0 +1,15 @@
+import { PhoenixV3Engine } from './core/engine.js';
+
+const canvas = document.getElementById('game');
+const engine = new PhoenixV3Engine(canvas);
+engine.boot();
+
+window.PHX_V3_ENGINE = engine;
+window.__PHX_V3_READY = true;
+
+const startBtn = document.getElementById('startBtn');
+const mapBtn = document.getElementById('mapBtn');
+startBtn?.addEventListener('click', () => engine.start());
+mapBtn?.addEventListener('click', () => engine.openMap());
+
+console.log('Phoenix7 v3 architecture prototype ready');
