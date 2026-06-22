3fcf4e154625feab6b93d5482af535e611d15061
severonsky-cloud <severonsky@gmail.com>
2026-06-22 22:28:10 +0300

    Bump cache after vehicle visual preservation fix

--- src/game/v3/main.js
+++ src/game/v3/main.js
@@ -1,18 +1,18 @@
-import { PhoenixV3Engine } from './core/engine.js?v=v3p2_visible_vehicles_1';
-import { installArsenalExtensions } from './core/engineArsenalExtensions.js?v=v3p2_visible_vehicles_1';
+import { PhoenixV3Engine } from './core/engine.js?v=v3p2_visible_vehicles_2';
+import { installArsenalExtensions } from './core/engineArsenalExtensions.js?v=v3p2_visible_vehicles_2';
 import { installSettlementExtensions } from './core/engineSettlementExtensions.js?v=30m2a_n1_daynight_integration_1';
-import { installArmedWorldExtensions } from './core/engineArmedWorldExtensions.js?v=v3p2_visible_vehicles_1';
+import { installArmedWorldExtensions } from './core/engineArmedWorldExtensions.js?v=v3p2_visible_vehicles_2';
 import { installAIFeelExtensions } from './core/engineAIFeelExtensions.js?v=30m2a_n1_daynight_integration_1';
-import { installActorVisualExtensions } from './core/engineActorVisualExtensions.js?v=30m2a_n1_daynight_integration_1';
+import { installActorVisualExtensions } from './core/engineActorVisualExtensions.js?v=v3p2_visible_vehicles_2';
 import { installFortEncounterExtensions } from './core/engineFortEncounterExtensions.js?v=30m2a_n1_daynight_integration_1';
-import { installFeelExtensions } from './core/engineFeelExtensions.js?v=v3p2_visible_vehicles_1';
+import { installFeelExtensions } from './core/engineFeelExtensions.js?v=v3p2_visible_vehicles_2';
 import { installPlayerHandsExtensions } from './core/enginePlayerHandsExtensions.js?v=30m2a_n1_daynight_integration_1';
 import { installPlayerBodyExtensions } from './core/enginePlayerBodyExtensions.js?v=30m2a_n1_daynight_integration_1';
 import { installAtmosphereExtensions } from './core/engineAtmosphereExtensions.js?v=30m2a_n1_daynight_integration_1';
 import { installCharacterExtensions } from './core/engineCharacterExtensions.js?v=30m2a_n1_daynight_integration_1';
 import { installDayNightExtensions } from './core/engineDayNightExtensions.js?v=30m2a_n1_daynight_integration_1';
-import { installWw2ArsenalExtensions } from './core/engineWw2ArsenalExtensions.js?v=v3p2_visible_vehicles_1';
-import { installWw2VehicleExtensions } from './core/engineWw2VehicleExtensions.js?v=v3p2_visible_vehicles_1';
+import { installWw2ArsenalExtensions } from './core/engineWw2ArsenalExtensions.js?v=v3p2_visible_vehicles_2';
+import { installWw2VehicleExtensions } from './core/engineWw2VehicleExtensions.js?v=v3p2_visible_vehicles_2';
 
 installArsenalExtensions(PhoenixV3Engine);
 installSettlementExtensions(PhoenixV3Engine);
