5a7e07596142cf77430cbe1e6867f497c5abe1f4
severonsky-cloud <severonsky@gmail.com>
2026-06-25 06:21:32 +0300

    Wire Act 1 vehicle repair pass

--- src/game/v3/core/engineAct1IntegratedExtensions.js
+++ src/game/v3/core/engineAct1IntegratedExtensions.js
@@ -1,9 +1,10 @@
-import { installAshgraveWindowExtensions } from './engineAshgraveWindowExtensions.js?v=v3p2_act1_repair_1';
-import { installAct1CompletionExtensions } from './engineAct1CompletionExtensions.js?v=v3p2_act1_repair_1';
-import { installEntityDebugExtensions } from './engineEntityDebugExtensions.js?v=v3p2_act1_repair_1';
-import { installAct1VehicleBridgeExtensions } from './engineAct1VehicleBridgeExtensions.js?v=v3p2_act1_repair_1';
-import { installAct1RoadEventsExtensions } from './engineAct1RoadEventsExtensions.js?v=v3p2_act1_repair_1';
-import { installAct1RuntimeRepairExtensions } from './engineAct1RuntimeRepairExtensions.js?v=v3p2_act1_repair_1';
+import { installAshgraveWindowExtensions } from './engineAshgraveWindowExtensions.js?v=v3p2_act1_vehicle_fix_1';
+import { installAct1CompletionExtensions } from './engineAct1CompletionExtensions.js?v=v3p2_act1_vehicle_fix_1';
+import { installEntityDebugExtensions } from './engineEntityDebugExtensions.js?v=v3p2_act1_vehicle_fix_1';
+import { installAct1VehicleBridgeExtensions } from './engineAct1VehicleBridgeExtensions.js?v=v3p2_act1_vehicle_fix_1';
+import { installAct1RoadEventsExtensions } from './engineAct1RoadEventsExtensions.js?v=v3p2_act1_vehicle_fix_1';
+import { installAct1RuntimeRepairExtensions } from './engineAct1RuntimeRepairExtensions.js?v=v3p2_act1_vehicle_fix_1';
+import { installAct1VehicleTargetRepairExtensions } from './engineAct1VehicleTargetRepairExtensions.js?v=v3p2_act1_vehicle_fix_1';
 
 export function installAct1IntegratedExtensions(PhoenixV3Engine) {
   installAshgraveWindowExtensions(PhoenixV3Engine);
@@ -12,4 +13,5 @@ export function installAct1IntegratedExtensions(PhoenixV3Engine) {
   installAct1VehicleBridgeExtensions(PhoenixV3Engine);
   installAct1RoadEventsExtensions(PhoenixV3Engine);
   installAct1RuntimeRepairExtensions(PhoenixV3Engine);
+  installAct1VehicleTargetRepairExtensions(PhoenixV3Engine);
 }
